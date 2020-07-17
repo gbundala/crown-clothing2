5216ffc71d07433ca9f36797cc912eb0da201d07
gbundala <gabrielbundala@gmail.com>
2020-07-17 03:25:03 +0300

    implemented cart utils to group items in cart reducer for qty eval, added new shop data with unique id for each item across

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import './collection-item.styles.scss'
+import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
+import { connect } from 'react-redux';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+    const { name, price, imageUrl } = item;
     return (
         <div className='collection-item'>
             <div 
@@ -14,8 +18,15 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
                 <span className='name'>{ name }</span>
                 <span className='price'>{ price }</span>
             </div>
+            <CustomButton inverted onClick={() => addItem(item)}>
+                Add to cart
+            </CustomButton>
         </div>
     );
 }
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
