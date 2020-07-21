029e62cab9aa699797b414c238fef14cf42ab8a4
gbundala <gabrielbundala@gmail.com>
2020-07-21 13:47:32 +0300

    added shop data and directory into its own reducers and created selected for them and added such to the respective components

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -3,7 +3,7 @@ import './collection-overview.styles.scss';
 
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { selectShopCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
 const CollectionOverview = ({ collections }) => {
@@ -17,7 +17,7 @@ const CollectionOverview = ({ collections }) => {
 };
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectShopCollections,
+    collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionOverview);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,14 +1,6 @@
 import { createSelector } from "reselect";
 import memoize from 'lodash.memoize';
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-};
-
 const selectShop = state => state.shop;
 
 const selectCollections = state => state.shop.collections; 
@@ -18,11 +10,14 @@ export const selectShopCollections = createSelector(
     shop => shop.collections
 );
 
+export const selectCollectionsForPreview = createSelector(
+    [selectShopCollections],
+    collections => Object.keys(collections).map(key => collections[key])//we get the value of our collection object at that key.
+);
+
 export const selectCollection = memoize(collectionUrlParam => createSelector(
     [selectCollections],
-    collections => collections.find(
-        collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+    collections => collections[collectionUrlParam]
 ));
 //we have memoized the outpout of this selector here due to the collectionUrlParam
 //In this case collectionUrlParam is a dynamic argument meaning it can change, 
