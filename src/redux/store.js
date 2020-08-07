bf4fe81b2ff266dfd1920ff413af0db9f709d039
gbundala <gabrielbundala@gmail.com>
2020-08-07 12:42:49 +0300

    completed implementation of root saga and shop saga just before shifting user into saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./root-reducer";
 import { persistStore } from "redux-persist";
 import createSagaMiddleware from "redux-saga";
-import { fetchCollectionsStart } from "./shop/shop.sagas";
+import rootSaga from "./root-saga";
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "development") {
 //hence the ...middlewares spread operator!
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-sagaMiddleware.run(fetchCollectionsStart);
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
